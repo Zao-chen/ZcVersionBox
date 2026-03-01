0639e9c450947469414a51643219f1bff8cc652d
Zao-chen <greatzaochen@outlook.com>
2026-03-01 16:35:52 +0800

    chore: add .gitignore and remove tracked build/IDE files

--- .vscode/build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Set up Visual Studio environment
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-
-REM Run CMake build
-"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe" --build "%~dp0\..\build\Desktop_Qt_6_6_3_MSVC2019_64bit-Release" --config Release
-
-exit /b %ERRORLEVEL%
