9e38eb28e9631b8268cfeb17d88612b8b030a6d0
Zao-chen <greatzaochen@outlook.com>
2026-05-21 22:19:11 +0800

    refactor: 优化 AiProvider 类的代码格式和结构

--- 3rdparty/ZcAILib/include/AiProvider.h
+++ 3rdparty/ZcAILib/include/AiProvider.h
@@ -11,66 +11,73 @@
 class QNetworkAccessManager;
 class QNetworkReply;
 
-class ZCAILIB_EXPORT AiProvider : public QObject {
-  Q_OBJECT
+class ZCAILIB_EXPORT AiProvider : public QObject
+{
+    Q_OBJECT
 
-public:
-  enum ServiceType { OpenAI, DeepSeek, Custom };
+  public:
+    enum ServiceType
+    {
+        OpenAI,
+        DeepSeek,
+        Custom
+    };
 
-  struct ModelInfo {
-    QString id;
-    QString created;
-    QString ownedBy;
-    QStringList permissions;
-  };
+    struct ModelInfo
+    {
+        QString id;
+        QString created;
+        QString ownedBy;
+        QStringList permissions;
+    };
 
-  explicit AiProvider(QObject *parent = nullptr);
-  ~AiProvider();
+    explicit AiProvider(QObject *parent = nullptr);
+    ~AiProvider();
 
-  void setServiceType(ServiceType type);
-  void setApiKey(const QString &apiKey);
-  void setApiUrl(const QString &url);
-  void setBaseUrl(const QString &baseUrl);
-  void setModel(const QString &model);
-  void setStreamEnabled(bool enabled);
+    void setServiceType(ServiceType type);
+    void setApiKey(const QString &apiKey);
+    void setApiUrl(const QString &url);
+    void setBaseUrl(const QString &baseUrl);
+    void setModel(const QString &model);
+    void setStreamEnabled(bool enabled);
 
-  QString currentModel() const { return m_model; }
-  ServiceType currentServiceType() const { return m_serviceType; }
-  bool isStreamEnabled() const { return m_streamEnabled; }
+    QString currentModel() const { return m_model; }
+    ServiceType currentServiceType() const { return m_serviceType; }
+    bool isStreamEnabled() const { return m_streamEnabled; }
 
-  void fetchModels();
-  void chat(const QString &message);
-  void setSystemPrompt(const QString &prompt);
+    void fetchModels();
+    void chat(const QString &message);
+    void setSystemPrompt(const QString &prompt);
 
-  QString m_systemPrompt;
+    QString m_systemPrompt;
 
-signals:
-  void replyReceived(const QString &reply);
-  void replyChunkReceived(const QString &chunk);
-  void errorOccurred(const QString &error);
-  void modelsReceived(const QList<ModelInfo> &models);
+  signals:
+    void replyReceived(const QString &reply);
+    void replyChunkReceived(const QString &chunk);
+    void errorOccurred(const QString &error);
+    void modelsReceived(const QList<ModelInfo> &models);
 
-private slots:
-  void handleReply();
-  void handleStreamReadyRead();
-  void handleModelsReply();
+  private slots:
+    void handleReply();
+    void handleStreamReadyRead();
+    void handleModelsReply();
 
-private:
-  void processStreamChunk(QNetworkReply *reply, const QByteArray &chunk);
-  void finalizeStreamReply(QNetworkReply *reply);
-  void cleanupStreamReply(QNetworkReply *reply);
+  private:
+    void processStreamChunk(QNetworkReply *reply, const QByteArray &chunk);
+    void finalizeStreamReply(QNetworkReply *reply);
+    void cleanupStreamReply(QNetworkReply *reply);
 
-  QNetworkAccessManager *m_network;
-  QString m_apiKey;
-  QString m_apiUrl;
-  QString m_baseUrl;
-  QString m_model;
-  bool m_streamEnabled;
-  QString m_modelsApiUrl;
-  ServiceType m_serviceType;
-  QHash<QNetworkReply *, QByteArray> m_streamBuffers;
-  QHash<QNetworkReply *, QByteArray> m_rawResponses;
-  QHash<QNetworkReply *, QString> m_streamReplies;
+    QNetworkAccessManager *m_network;
+    QString m_apiKey;
+    QString m_apiUrl;
+    QString m_baseUrl;
+    QString m_model;
+    bool m_streamEnabled;
+    QString m_modelsApiUrl;
+    ServiceType m_serviceType;
+    QHash<QNetworkReply *, QByteArray> m_streamBuffers;
+    QHash<QNetworkReply *, QByteArray> m_rawResponses;
+    QHash<QNetworkReply *, QString> m_streamReplies;
 };
 
 #endif // AIPROVIDER_H
